8a4819fb9f32018f54e81be8935fbdbcfca1db38
syntaxmonkey2px <eopqrs@gmail.com>
2025-03-06 21:17:01 +0000

    base line changed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fso-part3/',
+  base: '/',
   server: {
     proxy: {
       '/api': {
