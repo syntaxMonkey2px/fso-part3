723273cb8eb63323002251f92fdf781e5c638dcc
syntaxmonkey2px <eopqrs@gmail.com>
2025-03-06 15:01:45 +0000

    commented hello world out

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ let notes = [
   }
 ]
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!!</h1>')
-  })
+// app.get('/', (request, response) => {
+//     response.send('<h1>Hello World!!</h1>')
+//   })
 
 //   "/" = root
 app.get('/api/notes', (request, response) => {
