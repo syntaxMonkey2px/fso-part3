68df390b485fe3967958d00ebe00620ee94620ca
syntaxmonkey2px <eopqrs@gmail.com>
2025-03-07 18:14:03 +0000

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -14,6 +14,12 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
+      external: ['axios'],
     },
   },
-})
+  resolve: {
+    alias: {
+      axios: 'axios/dist/axios.min.js', // Adjust the path if necessary
+    },
+  },
+});
